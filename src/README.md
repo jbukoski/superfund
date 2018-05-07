2993c326f2ec9a49508c0701b970e52ee66802b1
jbukoski <jacob.bukoski@yale.edu>
2018-05-07 10:24:49 -0700

    Updating the src directory with readme documentation

---
+++ src/README.md
@@ -0,0 +1,5 @@
+### Documentation for `src` scripts
+
+This readme file provides documentation of the scripts that are housed within the `src` directory. A full listing of the scripts is provided as well as a  brief overview as to what they do.
+
+1. `get_data.R` -- A R script to download the necessary raw data files from stable URLs. The data files are downloaded to the `raw` folder and subsequently unzipped there.
--- src/get_data.R
+++ src/get_data.R
@@ -1,4 +1,7 @@
 # R script to download the raw datafiles from stable URLs
+# The script also extracts the data from the zip files
+
+library(utils)
 
 download.file("https://svi.cdc.gov/Documents/Data/2016_SVI_Data/SVI2016_US.zip",
               "~/projects/superfund/superfund/raw/svi2016_us.zip")
@@ -8,3 +11,12 @@ download.file("http://www2.census.gov/geo/tiger/GENZ2017/shp/cb_2017_us_county_2
 
 download.file("https://toxmap-classic.nlm.nih.gov/toxmap/download/superfund_shapefile.zip",
               "~/projects/superfund/superfund/raw/superfund_shapefile.zip")
+
+path <- "~/projects/superfund/superfund/raw/"
+
+
+for(i in list.files(path)) {
+  
+  unzip(paste0(path, i), exdir = path)
+
+  }
